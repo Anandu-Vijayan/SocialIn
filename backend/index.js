0bf42907fa316eed3503e5363136591831e3be9c
anandu <ananduvijayan8@gmail.com>
2022-09-15 14:04:27 +0530

    crreate post,update post,delete post,like post and unlike post backend function full finished

--- backend/index.js
+++ backend/index.js
@@ -5,6 +5,7 @@ import dotenv from "dotenv";
 import AuthRoute from './Routes/AuthRoute.js'
 import { errorHandler, notFound } from "./middilewares/errorMiddleware.js";
 import UserRoute from './Routes/UserRoute.js'
+import PostRoute from './Routes/PostRoute.js'
 
 
 
@@ -32,6 +33,7 @@ mongoose
 
 
   //usage of routes
-  app.use('/auth',AuthRoute)
-  app.use('/user',UserRoute)
+  app.use('/auth',AuthRoute) 
+  app.use('/user',UserRoute) 
+  app.use('/post',PostRoute)
    
