3701c8187d82dd531598a16ccd22a9f32fc2f41b
anandu <ananduvijayan8@gmail.com>
2022-09-15 17:55:48 +0530

    time views setup done in backend,and seeing in asending order

--- backend/Controllers/PostController.js
+++ backend/Controllers/PostController.js
@@ -1,5 +1,6 @@
 import PostModel from "../Models/postModel.js";
 import mongoose from "mongoose";
+import UserModel from "../Models/userModel.js";
 
 
 
@@ -79,18 +80,61 @@ export const likePost = async (req, res) => {
 
     try {
         const post = await PostModel.findById(id)
-        if (!post.likes.includes(userId))
-        {
-            await post.updateOne({$push :{likes:userId}})
+        if (!post.likes.includes(userId)) {
+            await post.updateOne({ $push: { likes: userId } })
             res.status(200).json("Post liked")
 
-        }else{
-            await post.updateOne({$pull :{likes:userId}})
+        } else {
+            await post.updateOne({ $pull: { likes: userId } })
             res.status(200).json("Post Unliked")
         }
 
     } catch (error) {
-        res.status(500)
+        res.status(500).json(error)
 
     }
 }
+
+//Get Timeline Posts
+
+export const getTimelinePosts = async (req, res) => {
+    const userId = req.params.id
+    console.log(userId);
+    try {
+        const currentUserPosts = await PostModel.find({ userId: userId })
+        const followingPosts = await UserModel.aggregate([
+            {
+                $match:{
+                    _id : new mongoose.Types.ObjectId (userId)
+                }
+
+            },
+            {
+                $lookup:{
+                    from:"posts",
+                    localField:"following",
+                    foreignField:"userId",
+                    as:"followingPosts"
+                }
+            },
+            {
+                $project:{
+                    followingPosts:1,
+                    _id:0
+                }
+            }
+
+        ])
+        res.status(200).json(currentUserPosts.concat(...followingPosts[0].followingPosts)
+        .sort((a,b)=>{
+            return b.createdAt - a.createdAt
+        })
+        )
+
+    } catch (error) {
+        res.status(500).json(error)
+
+    }
+
+}
+
--- backend/Routes/PostRoute.js
+++ backend/Routes/PostRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { createPost, deletePost, getPost, likePost, updatePost } from '../Controllers/PostController.js'
+import { createPost, deletePost, getPost, getTimelinePosts, likePost, updatePost } from '../Controllers/PostController.js'
 const router = express.Router()
 
 router.post('/',createPost)
@@ -7,6 +7,7 @@ router.get('/:id',getPost)
 router.put('/:id',updatePost)
 router.delete('/:id',deletePost)
 router.put('/:id/like',likePost)
+router.get('/:id/timeline',getTimelinePosts) 
 
 
 
