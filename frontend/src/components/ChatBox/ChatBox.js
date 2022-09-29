33bffc6d5db36605aa86f38a39fca987d0340371
anandu <ananduvijayan8@gmail.com>
2022-09-29 15:55:18 +0530

    Chat front end and backend complete,Button option and sent options are pending

--- frontend/src/components/ChatBox/ChatBox.js
+++ frontend/src/components/ChatBox/ChatBox.js
@@ -1,32 +1,105 @@
-import React, { useEffect, useState } from 'react'
-import { getUser } from '../../api/UserRequest'
+import React, { useEffect, useState } from "react";
+import { getMessages } from "../../api/MessageRequests";
+import { getUser } from "../../api/UserRequest";
+import "./ChatBox.css";
+import {format} from "timeago.js"
+import InputEmoji from "react-input-emoji"
 
-const ChatBox = ({chat,currentUser}) => {
-    const [userData,setUserData] = useState(null)
+const ChatBox = ({ chat, currentUser }) => {
+  const [userData, setUserData] = useState(null);
+  const [messages,setMessages]= useState([]);
+  const [newMessage, setNewMessage] = useState("");
 
+  //fetching data for header
+  useEffect(() => {
+    const userId = chat?.members.find((id) => id !== currentUser);
+    const getUserData = async () => {
+      try {
+        const { data } = await getUser(userId);
+        setUserData(data);
+        console.log(data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (chat !== null) getUserData();
+  }, [chat, currentUser]);
 
-    // fetching dat for header
-    useEffect(()=>{
-        const userId = chat?.memebers?.find((id)=> id !==currentUser)
-        const getUserData = async ()=>{
-            try {
-                const {data} = await getUser (userId)
-                setUserData(data);
-                console.log(data);
-            } catch (error) {
-                console.log(error)
-            }
+  //fetching data from messages
+
+  useEffect(()=>{
+    const fetchMessages = async ()=> {
+        try {
+            const {data} = await getMessages(chat._id)
+            console.log(data);
+            setMessages(data);
+        } catch (error) {
+            console.log(error)
         }
-        if(chat !==null) getUserData()
+    }
+    if (chat !== null) fetchMessages();
+  },[chat])
 
-    },[chat,currentUser])
+   const handleChange =(newMessage)=>{
+    setNewMessage(newMessage)
+   }
+
   return (
-   <>
-   <div className='ChatBox-container'>
+    <>
+      <div className="ChatBox-container">
 
-   </div>
-   </>
-  )
-}
+        <>
+          <div className="chat-header">
+            <div className="followers">
+              <div>
+                <div className="online-dot"></div>
+                <img
+                  src={
+                    userData?.profilePicture
+                      ? process.env.REACT_APP_PUBLIC_FOLDER +
+                        userData.profilePicture
+                      : process.env.REACT_APP_PUBLIC_FOLDER + "profile.png"
+                  }
+                  alt=""
+                  className="followerImage"
+                  style={{ width: "50px", height: "50px" }}
+                />
+                <div className="name" style={{ fontSize: "0.8rem" }}>
+                  <span>
+                    {userData?.firstname} {userData?.lastname}
+                  </span>
+                </div>
+              </div>
+            </div>
+            <hr style={{width:'99%',border:'0.1px solid #ececec'}}/>
+          </div>
+          {/* chatbox messages */}
+          {messages.map((message)=>(
+            <>
+            <div className={message.senderId === currentUser ? "message own":"message"}>
+                <span>{message.text}</span>
+                <span>{format(message.createdAt)}</span>
 
-export default ChatBox
+            </div>
+            </>
+          ))}
+
+        </>
+      </div>
+      {/* chats-sender */}
+      <div className="chat-sender">
+        <div>+</div>
+        <InputEmoji
+        value={newMessage}
+        onChange={handleChange}
+        />
+        <div className="send-button button">
+            Send
+        </div>
+    
+      </div>
+    </>
+  );
+};
+
+export default ChatBox;
--- frontend/src/pages/Chat/Chat.js
+++ frontend/src/pages/Chat/Chat.js
@@ -1,48 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { userChats } from "../../api/ChatRequest";
-import { UilSetting } from '@iconscout/react-unicons';
-import Home from '../../img/Home.png'
-import Noti from '../../img/Noti.png'
-import Comment from '../../img/Comment.png'
+import { UilSetting } from "@iconscout/react-unicons";
+import Home from "../../img/Home.png";
+import Noti from "../../img/Noti.png";
+import Comment from "../../img/Comment.png";
 import Conversation from "../../components/Conversation/Conversation";
 import LogoSearch from "../../components/LogoSearch/LogoSearch";
-import './Chat.css'
-import { Link } from 'react-router-dom'
+import "./Chat.css";
+import { Link } from "react-router-dom";
 import ChatBox from "../../components/ChatBox/ChatBox";
 
 const Chat = () => {
   const { user } = useSelector((state) => state.authReducer.authData);
 
-
-  const [chats, setChats] = useState([])
-  const [currentChat,setCurrentChat]=useState(null)
+  const [chats, setChats] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
 
   useEffect(() => {
     const getChats = async () => {
       try {
-        const { data } = await userChats(user._id)
-        setChats(data)
+        const { data } = await userChats(user._id);
+        setChats(data);
         console.log(data);
       } catch (error) {
         console.log(error);
-
       }
-    }
-    getChats()
-
-  }, [])
+    };
+    getChats();
+  }, [user._id]);
   return (
     <div className="Chat">
       {/*Left Side*/}
       <div className="Left-side-chat">
         <LogoSearch />
         <div className="Chat-container">
-
           <h2>Chats</h2>
           <div className="Chat-list">
             {chats.map((chat) => (
-              <div onClick={()=> setCurrentChat(chat)}>
+              <div onClick={() => setCurrentChat(chat)}>
                 <Conversation data={chat} currentUser={user._id} />
               </div>
             ))}
@@ -51,21 +47,22 @@ const Chat = () => {
       </div>
       {/*Right Side*/}
       <div className="Right-side-chat">
-        <div style={{ width: '20rem', alignSelf: 'flex-end' }}>
-          <div className='navIcons'>
-            <Link to='../home'>
+        <div style={{ width: "20rem", alignSelf: "flex-end" }}>
+          <div className="navIcons">
+            <Link to="../home">
               <img src={Home} alt="" />
             </Link>
             <UilSetting />
             <img src={Noti} alt="" />
-            <Link to='../chat'>
+            <Link to="../chat">
               <img src={Comment} alt="" />
             </Link>
           </div>
-          {/*chat body*/ }
-          {/* <ChatBox chat ={currentChat} currentUser={user._id}/> */}
+          {/*chat body*/}
         </div>
-
+        <div>
+          <ChatBox chat={currentChat} currentUser={user._id} />
+        </div>
       </div>
     </div>
   );
